65395bd3e5e3e652666dca98b294d6bff2aa8d03
志雄 <pzxtemp@sina.com>
2016-08-23 00:30:54 +0800

    插入排序

--- src/main/java/allSort.java
+++ src/main/java/allSort.java
@@ -3,20 +3,45 @@ class allSort {
     public static void main(String[] args){
         int[] array = new int[10000];
         init(array);
+        //print(array);
         //排序区
         long start_time=System.currentTimeMillis();
 
-        bubble_sort(array);//冒泡排序---219ms
+        //bubble_sort(array);//冒泡排序---219ms
         //selection_sort(array);//选择排序---129ms
-
+        insert_sort(array);//插入排序
         long end_time=System.currentTimeMillis();
         System.out.println("排序前："+start_time);
-        System.out.println("排序前："+end_time);
+        System.out.println("排序后："+end_time);
         System.out.println("用时："+(end_time-start_time));
 
-        print(array);
+        //print(array);
     }
 
+
+    //插入排序
+    public static void insert_sort(int[] array){
+        if(array[1]<array[0]){
+            int temp=array[0];
+            array[0]=array[1];
+            array[1]=temp;
+        }
+        for (int i=2;i<array.length;i++){
+            for (int j=0;j<i;j++){
+                if(array[i]<array[j]) {
+                    insert(array, i, j, array[i]);
+                    break;
+                }
+            }
+        }
+    }
+
+    public static void insert(int[] array , int index , int insertPoint, int value){
+        for(int i=index;i>insertPoint;i--){
+            array[i]=array[i-1];
+        }
+        array[insertPoint]=value;
+    }
 
     //选择排序
     public static void selection_sort(int[] array){
@@ -62,5 +87,6 @@ class allSort {
         for(int i=0;i<array.length;i++){
             System.out.print(array[i]+"  ");
         }
+        System.out.println();
     }
 }
